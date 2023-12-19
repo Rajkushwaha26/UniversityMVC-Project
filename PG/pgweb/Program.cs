5c84c1bd95382200117b017be264f24cf0711d1b
Iamneo <techadmin@iamneo.ai>
2023-12-19 13:41:29 +0000

    Auto-save-commit-19/12/2023--19:11:29

--- PG/pgweb/Program.cs
+++ PG/pgweb/Program.cs
@@ -1,14 +1,13 @@
 using pgweb.Models;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(Options=>{
-    Options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
-});
+ builder.Services.AddDbContext<ApplicationDbContext>(Options=>{
+     Options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+ });
 
 var app = builder.Build();
 
@@ -32,3 +31,34 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// builder.Services.AddControllersWithViews();
+
+// builder.Services.AddDbContext<ApplicationDbContext>(Options=>{
+//     Options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+// });
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
+
+// app.UseRouting();
+
+// app.UseAuthorization();
+
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// app.Run();
